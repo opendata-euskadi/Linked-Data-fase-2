262dd45651a6f7e7441198d4daf054a332dad063
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2021-05-30 22:14:51 +0200

    RDF4J JUnit

---
+++ LDf2/RDF4JGraphDB/src/test/java/eus/ehu/ldf2/RDF4JGraphDBConnection.java
@@ -0,0 +1,24 @@
+package eus.ehu.ldf2;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RDF4JGraphDBConnection {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
