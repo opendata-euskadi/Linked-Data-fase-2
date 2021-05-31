ef8a105f16932fa59a91de8b316f540aed169b6f
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2021-05-31 16:59:32 +0200

    RDF4J connection works

--- LDf2/RDF4JGraphDB/src/test/java/eus/ehu/ldf2/RDF4JGraphDBConnection.java
+++ LDf2/RDF4JGraphDB/src/test/java/eus/ehu/ldf2/RDF4JGraphDBConnection.java
@@ -2,56 +2,54 @@ package eus.ehu.ldf2;
 
 import static org.junit.Assert.*;
 
-
-import static org.eclipse.rdf4j.model.util.Statements.statement;
-import static org.eclipse.rdf4j.model.util.Values.iri;
-import static org.eclipse.rdf4j.model.util.Values.literal;
-
 import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Literal;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.ValueFactory;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
-
-
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.Namespace;
+import org.eclipse.rdf4j.model.util.Values;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.query.BooleanQuery;
+import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.manager.RemoteRepositoryManager;
 import org.eclipse.rdf4j.repository.manager.RepositoryManager;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+
+
 import org.junit.Test;
 
 public class RDF4JGraphDBConnection {
 
 	@Test
-	public void testRemoteRepo() {
+	public void testAddTriple() {
 		RepositoryManager repositoryManager = new RemoteRepositoryManager("http://127.0.0.1:7200");
 		
-		// Get the repository from repository manager
 		Repository repository = repositoryManager.getRepository("Test-RDF4J");
 		
-//		http://10.107.17.182:7200/repositories/Test-RDF4J
-
-		// Open a connection to this repository
 		RepositoryConnection repositoryConnection = repository.getConnection();
 		
-		// Create statement (Triple)
-		ValueFactory factory = SimpleValueFactory.getInstance();
-		IRI bob = iri(factory, "http://example.org/bob");
-		IRI name = iri(factory,"http://example.org/name");
-		Literal bobsName = literal(factory, "Bob");
-		Statement nameStatement = statement(factory, bob, name, bobsName);
+		Namespace ex = Values.namespace("ex", "http://example.org/");
+		IRI john = Values.iri(ex, "john");
+		IRI namedgraph = Values.iri("http://id.euskadi.eus/graph/rdf4j-test");
+		
+		repositoryConnection.add(john, RDF.TYPE, FOAF.PERSON, namedgraph);
+		repositoryConnection.add(john, RDFS.LABEL, Values.literal("John"), namedgraph);
+		
+		StringBuilder queryBuilder = new StringBuilder();
+		queryBuilder.append("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>");
+		queryBuilder.append("PREFIX ex: <http://example.org/>");
+		queryBuilder.append("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>");
+		queryBuilder.append("PREFIX foaf: <http://xmlns.com/foaf/0.1/>");
+		queryBuilder.append("ASK");
+		queryBuilder.append("WHERE { GRAPH <http://id.euskadi.eus/graph/rdf4j-test> {ex:john rdfs:label \"John\" ;");
+		queryBuilder.append("rdf:type foaf:Person . }}");
+			      
+		BooleanQuery query = repositoryConnection.prepareBooleanQuery(QueryLanguage.SPARQL, queryBuilder.toString());
+		
+		assertTrue(query.evaluate());
 
-
-
-
-		// Shutdown connection, repository and manager
 		repositoryConnection.close();
 		repository.shutDown();
 		repositoryManager.shutDown();
-
 	}
-
 }
